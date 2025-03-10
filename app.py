ba43eeda06df8bd19942926bcf671afb92549576
Fatimah Alzahrani <12fatimah.15@gmail.com>
2025-03-11 02:03:01 +0300

    .

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
 from transformers import pipeline
+import os
 
 app = Flask(__name__)
 
@@ -128,5 +129,6 @@ def upload_file():
     else:
         return 'الرجاء تحميل ملف Excel صحيح.'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
